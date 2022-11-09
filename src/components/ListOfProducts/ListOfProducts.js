08d346755b1bd7a19f742aa08222ce9d512fa306
Darya <daryapleshko.work.dev@mail.ru>
2022-11-09 22:49:00 +0300

    commit

--- src/components/ListOfProducts/ListOfProducts.js
+++ src/components/ListOfProducts/ListOfProducts.js
@@ -3,7 +3,7 @@ import listOfProducts from './css/ListOfProducts.module.css';
 import data from '../../context/products';
 
 const ListOfProducts = () => {
-   return (
+  return (
     <div id='catalog-id' className={listOfProducts['courses']}>
       {data && data.map((item) => <ProductItem key={item.id} id={item.id} title={item.title} path={item.path} price={item.price} {...item} />)}
     </div>
