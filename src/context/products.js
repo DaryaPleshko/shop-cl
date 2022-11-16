a75aac7f415d3216f7e36bb8d1ea2755f98e31fe
Darya <daryapleshko.work.dev@mail.ru>
2022-11-16 20:30:23 +0300

    commit

--- src/context/products.js
+++ src/context/products.js
@@ -40,6 +40,69 @@ const data = [
         path: 'coffee.jpg',
         price: 150,
         count: 0
+    },
+    {
+        id: 7,
+        title: 'Microwave',
+        path: 'microwave.jpg',
+        price: 250,
+        count: 0
+    },
+    {
+        id: 8,
+        title: 'Teapot',
+        path: 'teapot.webp',
+        price: 150,
+        count: 0
+    },
+    {
+        id: 9,
+        title: 'Vacuum cleaner',
+        path: 'vacuum-cleaner.webp',
+        price: 300,
+        count: 0
+    },
+    {
+        id: 10,
+        title: 'Iphone 14',
+        path: 'iphone.webp',
+        price: 1200,
+        count: 0
+    },
+    {
+        id: 11,
+        title: 'Electric gril',
+        path: 'electric-gril.webp',
+        price: 500,
+        count: 0  
+    },
+    {
+        id: 12,
+        title: 'Frying pan',
+        path: 'frying-pan.jpg',
+        price: 300,
+        count: 0  
+    },
+    {
+        id: 13,
+        title: 'Multicooker',
+        path: 'multicooker.jpg',
+        price: 600,
+        count: 0  
+    },
+    {
+        id: 14,
+        title: 'Waffle iron',
+        path: 'waffle-iron.jpg',
+        price: 100,
+        count: 0  
+    },
+    {
+        id: 15,
+        title: 'AirPods Pro 2',
+        path: 'airpods.jpg',
+        price: 200,
+        count: 0  
     }
 ]
 
